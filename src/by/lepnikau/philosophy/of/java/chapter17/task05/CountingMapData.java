91a8e176d871aeb380cbc8f3c9a1ad8332464ee9
ViachaslauL <Lotussutol@tut.by>
2021-09-08 09:47:42 +0300

    task05 finished

--- src/by/lepnikau/philosophy/of/java/chapter17/task05/CountingMapData.java
+++ src/by/lepnikau/philosophy/of/java/chapter17/task05/CountingMapData.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class CountingMapData extends AbstractMap<Integer, String> {
     private int size;
-    private static String[] chars =
+    private static final String[] CHARS =
             "А В С D E F G H I 3 К L M N 0 P Q R S T U V W X Y Z"
                     .split(" ");
 
@@ -26,7 +26,7 @@ class CountingMapData extends AbstractMap<Integer, String> {
 
         @Override
         public String getValue() {
-            return chars[index % chars.length] + index / chars.length;
+            return CHARS[index % CHARS.length] + index / CHARS.length;
         }
 
         @Override
@@ -45,27 +45,60 @@ class CountingMapData extends AbstractMap<Integer, String> {
         }
     }
 
-    static class EntrySet extends AbstractSet<Map.Entry<Integer, String>> {
+    private static class EntrySet extends AbstractSet<Map.Entry<Integer, String>> {
         private int size;
+        private int index;
+        private Entry[] entries = new Entry[16];
+        private static final float EXPAND_COEFFICIENT = 1.5f;
 
         EntrySet(int size) {
             this.size = size;
+            if (size > entries.length) {
+                expandEntries();
+            }
         }
 
         @Override
         public Iterator<Map.Entry<Integer, String>> iterator() {
-            return null;
+            return new Iterator<Map.Entry<Integer, String>>() {
+
+                @Override
+                public boolean hasNext() {
+                    return index < size;
+                }
+
+                @Override
+                public Map.Entry<Integer, String> next() {
+                    return entries[index++];
+                }
+            };
         }
 
         @Override
         public int size() {
-            return 0;
+            return size;
+        }
+
+        public void add(Entry entry) {
+            for (int i = 0; i < entries.length; i++) {
+                if (entries[i] == null) {
+                    entries[i] = entry;
+                    break;
+                }
+            }
+        }
+
+        private void expandEntries() {
+            Entry[] tempEntries =
+                    new Entry[(int) (entries.length * EXPAND_COEFFICIENT)];
+            System.arraycopy(entries, 0, tempEntries, 0, entries.length);
+            entries = tempEntries;
         }
     }
 
     @Override
     public Set<Map.Entry<Integer, String>> entrySet() {
-        LinkedHashSet<Map.Entry<Integer, String>> entries = new LinkedHashSet<>();
+        EntrySet entries = new EntrySet(size);
         for (int i = 0; i < size; i++) {
             entries.add(new Entry(i));
         }
--- src/by/lepnikau/philosophy/of/java/chapter17/task05/CountingMapDataTest.java
+++ src/by/lepnikau/philosophy/of/java/chapter17/task05/CountingMapDataTest.java
@@ -1,8 +1,14 @@
 package by.lepnikau.philosophy.of.java.chapter17.task05;
 
+import java.util.Map;
+import java.util.Set;
+
 class CountingMapDataTest {
     public static void main(String[] args) {
-        CountingMapData countingMapData = new CountingMapData(10);
-
+        CountingMapData countingMapData = new CountingMapData(20);
+        Set<Map.Entry<Integer, String>> entries = countingMapData.entrySet();
+        for (Map.Entry<Integer, String> entry : entries) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
