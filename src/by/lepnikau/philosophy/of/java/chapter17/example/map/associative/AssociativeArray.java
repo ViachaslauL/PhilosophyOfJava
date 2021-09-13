b864926c4a9840f628eb5915ac3d599cb90e9fe7
ViachaslauL <Lotussutol@tut.by>
2021-09-13 18:55:05 +0300

    added task13 folder

---
+++ src/by/lepnikau/philosophy/of/java/chapter17/example/map/associative/AssociativeArray.java
@@ -0,0 +1,4 @@
+package by.lepnikau.philosophy.of.java.chapter17.example.map.associative;
+
+public class AssociativeArray {
+}
---
+++ src/by/lepnikau/philosophy/of/java/chapter17/task13/WordsMap.java
@@ -0,0 +1,4 @@
+package by.lepnikau.philosophy.of.java.chapter17.task13;
+
+public class WordsMap {
+}
