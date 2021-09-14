bc78281adf1b1c88c1ef12a7f2261cfb00a34e49
ViachaslauL <Lotussutol@tut.by>
2021-09-14 11:37:56 +0300

    task13 finished

--- src/by/lepnikau/philosophy/of/java/chapter17/task13/WordsMap.java
+++ src/by/lepnikau/philosophy/of/java/chapter17/task13/WordsMap.java
@@ -1,8 +1,17 @@
 package by.lepnikau.philosophy.of.java.chapter17.task13;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 class WordsMap<K, V> {
-    private Object[][] pairs;
+    private Object[][] pairs = new Object[16][2];
     private int index;
+    private static final float EXP_C = 1.5f;
+
+    public WordsMap() {
+    }
 
     public WordsMap(int length) {
         pairs = new Object[length][2];
@@ -10,7 +19,7 @@ class WordsMap<K, V> {
 
     public void put(K key, V val) {
         if (index >= pairs.length) {
-            throw new ArrayIndexOutOfBoundsException();
+            expandArray();
         }
         pairs[index++] = new Object[]{key, val};
     }
@@ -23,6 +32,19 @@ class WordsMap<K, V> {
         return null;
     }
 
+    @SuppressWarnings("unchecked")
+    public Set<K> keySet() {
+        Object[] keysArray = new Object[index];
+        for (int i = 0; i < index; i++) {
+            keysArray[i] = pairs[i][0];
+        }
+        return new HashSet<>((Collection<? extends K>) Arrays.asList(keysArray));
+    }
+
+    public int size() {
+        return index + 1;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -32,5 +54,9 @@ class WordsMap<K, V> {
             sb.append(pair[1].toString()).append("\n");
         }
         return sb.toString();
+    }
+
+    private void expandArray() {
+        pairs = Arrays.copyOf(pairs, (int) (pairs.length * EXP_C));
     }
 }
