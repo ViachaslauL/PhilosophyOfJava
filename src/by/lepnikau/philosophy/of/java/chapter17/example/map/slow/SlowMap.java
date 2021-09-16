d9b0ffa9861f961b77c03534f8c54c4b382cadea
ViachaslauL <Lotussutol@tut.by>
2021-09-16 08:09:25 +0300

    added exersize SlowMap and task16

--- src/by/lepnikau/philosophy/of/java/chapter17/example/map/slow/SlowMap.java
+++ src/by/lepnikau/philosophy/of/java/chapter17/example/map/slow/SlowMap.java
@@ -1,4 +1,47 @@
 package by.lepnikau.philosophy.of.java.chapter17.example.map.slow;
 
-public class SlowMap {
+import by.lepnikau.philosophy.of.java.util.countries.Countries;
+
+import java.util.*;
+
+public class SlowMap<K, V> extends AbstractMap<K, V> {
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+
+    public V put(K key, V val) {
+        V oldVal = get(key);
+        if (!keys.contains(key)) {
+            keys.add(key);
+            values.add(val);
+        } else {
+            values.set(keys.indexOf(key), val);
+        }
+        return oldVal;
+    }
+
+    public V get(Object key) {
+        if (!keys.contains(key)) {
+            return null;
+        }
+        return values.get(keys.indexOf(key));
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> set = new HashSet<>();
+        Iterator<K> kIter = keys.iterator();
+        Iterator<V> vIter = values.iterator();
+        while (kIter.hasNext()) {
+            set.add(new MapEntry<>(kIter.next(), vIter.next()));
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SlowMap<String, String> slowMap = new SlowMap<>();
+        slowMap.putAll(Countries.capitals(15));
+        System.out.println(slowMap);
+        System.out.println(slowMap.get("ANGOLA"));
+        System.out.println(slowMap.entrySet());
+    }
 }
--- src/by/lepnikau/philosophy/of/java/chapter17/task16/TaskSixteenTest.java
+++ src/by/lepnikau/philosophy/of/java/chapter17/task16/TaskSixteenTest.java
@@ -1,4 +1,18 @@
 package by.lepnikau.philosophy.of.java.chapter17.task16;
 
+import by.lepnikau.philosophy.of.java.chapter17.example.map.maps.Maps;
+import by.lepnikau.philosophy.of.java.chapter17.example.map.slow.SlowMap;
+import by.lepnikau.philosophy.of.java.util.countries.Countries;
+
 public class TaskSixteenTest {
+    public static void main(String[] args) {
+        SlowMap<Integer, String> slowMap = new SlowMap<>();
+        slowMap.put(1, "value one");
+        slowMap.put(2, "value two");
+        slowMap.put(3, "value three");
+        slowMap.put(4, "value four");
+        slowMap.put(5, "value five");
+
+        Maps.printKeys(slowMap);
+    }
 }
